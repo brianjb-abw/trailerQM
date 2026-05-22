59a3c43a47865588f44c427a113a732083eb0cd0
brianjb-abw <brianjb-tek@proton.mem>
2026-05-22 15:09:43 -0400

    moved source and output folders out of project dir

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
 import csv
 from pathlib import Path
+from dotenv import load_dotenv
 
 from handlers.AR import proc_AR
 from handlers.BB import proc_BB
 from handlers.LT import proc_LT
 from handlers.TC import proc_TC
+
+load_dotenv()
+
 
 HANDLERS = {
     'AR': proc_AR,
@@ -15,13 +20,19 @@ HANDLERS = {
 
 
 def main():
-    t_in = Path('T_IN')
-    csv_path = str(Path('T_OUT') / 'trailer_data.csv')
+    BASE_PATH = os.getenv("BASE_PATH")
+    in_dir = "tm_IN"
+    out_dir = "tm_OUT"
+    out_file = "tm_data.csv"
+
+    t_in = Path(os.path.join(BASE_PATH, in_dir))
+    csv_path = Path(os.path.join(BASE_PATH, out_dir, out_file))
 
     with open(csv_path, 'w', newline='') as f:
         csv.writer(f).writerow(['VIN', 'W', 'L', 'Description', 'Color', 'Cost'])
 
     for pdf_file in sorted(t_in.glob('*.pdf')):
+        print(f"filename: {pdf_file.name}")
         prefix = pdf_file.stem[:2].upper()
         handler = HANDLERS.get(prefix)
         if handler is None:
